1f479a53c53542f6c0b229de1dd45cb981121982
myshagodhiya <80570084+myshagodhiya@users.noreply.github.com>
2022-04-14 19:37:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,4 +17,24 @@ function take_snapshot()
 }
 console.log('ml5 version', ml5.version);
 
-classifier= ml5.imageClassifier ('https://teachablemachine.withgoogle.com/models/-PAlaoZsY/');
+classifier= ml5.imageClassifier ('https://teachablemachine.withgoogle.com/models/-PAlaoZsY/model.json');
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+}
+
+function check(){
+    img=document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+ if (error){
+     console.error(error);
+ }
+ else{
+     console.log(results);
+     document.getElementById("result_object_name").innerHTML = results[0].label;
+     document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+ }
+}
